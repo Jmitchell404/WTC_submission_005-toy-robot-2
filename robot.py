c25c4a99750a7329dd6dcf08399db2f7d600d7cd
jmitchel020 <jmitchel020@student.wethinkcode.co.za>
2021-12-01 11:26:45 +0200

    Toyrobot iteration 2

--- robot.py
+++ robot.py
@@ -1,87 +1,26 @@
-import os
-import sys
-dirc = ['forward', 'right', 'back', 'left']
-x = 0
-y = 0
-current_rotation = "forward"
-min_y, max_y = -201, 201
-min_x, max_x = -101, 101
-# rotation_var =-1
-# position= ({'x':0, 'y':1})
-def robot_start():
-    """This is the entry function, do not change"""
+def name_the_robot():
+    """
+    name of the robot is asked
+    name is stored and returned
+    """
     robot_name = input("What do you want to name your robot? ")
-    print(robot_name + ": Hello kiddo!" )
-    commands(robot_name)
+    print(robot_name + ": Hello kiddo!")
     return robot_name
 
 
-def commands(robot_name):
-    global x,y
-    answer = input(robot_name + ": What must I do next? ").lower()
-    if answer == "off":
-        print(robot_name + ": Shutting down..")
-        return
-
-    elif answer == "help":
-        print("""I can understand these commands:
- OFF - Shut down robot
- HELP - provide information about commands
- RIGHT - Puts the robot in a 90 angle to the right.
- LEFT - Puts the robot in a 90 angle to the left.
-""")
-        robot_move(robot_name, answer)
-        commands(robot_name)
-    elif "forward" in answer or "back" in answer:
-        # print(f" > {robot_name} moved forward by  steps.")
-        robot_move(robot_name, answer)
-        return commands(robot_name)
-
-    elif "right" in answer or "left" in answer:
-        robot_move(robot_name, answer)
-        print(f" > {robot_name} turned {answer}.")
-        print(f" > {robot_name} now at position ({x},{y}).")
-        #robot_left_or_right(answer, robot_name)
-        return commands(robot_name)
-    else:
-        print(f"{robot_name}: Sorry, I did not understand '{answer.upper}'.")
-        return commands(robot_name)
-    return commands(robot_name)
-
-
-def move_forward(input_from_user, robot_name, dirc, x, y):
+def get_command_input(robot_name):
     """
-    since the robot moves on a cartesian plane, x is for x-axis and y is for y-axis
-    the instruction(command) and steps(value) is stored in a tuple named stored_movements
-    stored_movements will then determine the direction and number of steps the robot should move
+    print statement asking user what the robot will do next appears
+    user input coverted into casefold and returned
     """
-    oldy = y
-    oldx = x
-    instruction = input_from_user.split()[0]
-    steps = int(input_from_user.split()[1])
-    stored_movements = (instruction, steps) 
-    if dirc == 1:
-        y = oldy + int(steps)
-        if y in range(-200, 200):
-            print(" > {} moved forward by {} steps.".format(robot_name, steps))
-    elif dirc == 0 or dirc== 4:
-        x = oldx - int(steps)
-        if x in range(-100,100):
-            print(" > {} moved forward by {} steps.".format(robot_name, steps))
-    elif dirc == 2 or dirc == -2:
-        x = oldx + int(steps)
-        if x in range(-100, 100):
-            print(" > {} moved forward by {} steps.".format(robot_name, steps))
-    elif dirc == -1 or dirc == 3:
-        y = oldy - int(steps)
-        if y in range(-200, 200):
-            print(" > {} moved forward by {} steps.".format(robot_name, steps))
-    return x, y, dirc, instruction, oldx, oldy
+    user_input =  input(f"{robot_name}: What must I do next? ")
+    new_input = user_input.casefold()
+    return new_input, user_input
 
 
-def move_backward(input_from_user, robot_name, dirc, x, y):
+def moving_forward(input_from_user, robot_name, direction, x, y):
     """
-    since the robot moves on a cartesian plane, x is for x-axis and y is for y-axis
+    x is for x-axis and y is for y-axis
     the instruction(command) and steps(value) is stored in a tuple named same
     same will then determine the direction and number of steps the robot should move
     """
@@ -89,61 +28,80 @@ def move_backward(input_from_user, robot_name, dirc, x, y):
     oldx = x
     instruction = input_from_user.split()[0]
     steps = int(input_from_user.split()[1])
-    stored_movements = (instruction, steps)
-    if dirc == 1:
+    same = (instruction, steps) 
+    if direction == 1:
+        y = oldy + int(steps)
+        if y in range(-200, 200):
+            print(" > {} moved forward by {} steps.".format(robot_name, steps))
+    elif direction == 0 or direction == 4:
+        x = oldx - int(steps)
+        if x in range(-100,100):
+            print(" > {} moved forward by {} steps.".format(robot_name, steps))
+    elif direction == 2 or direction == -2:
+        x = oldx + int(steps)
+        if x in range(-100, 100):
+            print(" > {} moved forward by {} steps.".format(robot_name, steps))
+    elif direction == -1 or direction == 3:
+        y = oldy - int(steps)
+        if y in range(-200, 200):
+            print(" > {} moved forward by {} steps.".format(robot_name, steps))
+    return x, y, direction, instruction, oldx, oldy
+
+
+def moving_backward(input_from_user, robot_name, direction, x, y):
+    """
+    x is for x-axis and y is for y-axis
+    the instruction(command) and steps(value) is stored in a tuple named same
+    same will then determine the direction and number of steps the robot should move
+    """
+    oldy = y
+    oldx = x
+    instruction = input_from_user.split()[0]
+    steps = int(input_from_user.split()[1])
+    same = (instruction, steps)
+    if direction == 1:
         y = y - int(steps)
         print(" > {} moved back by {} steps.".format(robot_name, steps))
-    elif dirc == 0 or dirc == 4:
+    elif direction == 0 or direction == 4:
         x = x + int(steps)
         print(" > {} moved back by {} steps.".format(robot_name, steps))
-    elif dirc == 2 or dirc == -2:
+    elif direction == 2 or direction == -2:
         x = x - int(steps)
         print(" > {} moved back by {} steps.".format(robot_name, steps))
-    elif dirc == -1 or dirc== 3:
+    elif direction == -1 or direction == 3:
         y = y + int(steps)
         print(" > {} moved back by {} steps".format(robot_name, steps))
-    return x, y, dirc, instruction, oldx, oldy
+    return x, y, direction, instruction, oldx, oldy
 
 
-def show_position(robot_name):
-    print(' > '+robot_name+' now at position ('+str(x)+','+str(y)+').')
+def keep_track_of_position(x, y, steps, robot_name, direction, oldx, oldy):
+    """
+    this function soley focuses on the position of the robot
+    current position is stored and a print statement will be printed
+    otherwise
+    """
+    if x > -101 and x < 101 and y > -201 and y < 201:
+        print(" > {} now at position ({},{}).".format(robot_name, x, y))  
+    else:
+        print("{}: Sorry, I cannot go outside my safe zone.".format(robot_name))
+        print(f" > {robot_name} now at position ({oldx},{oldy}).")
+        return oldx, oldy, direction
+    return x, y, direction
 
-# def robot_left_or_right(answer, robot_name):
-#     global current_rotation
-#     current_rotation == 
-def is_position_allowed(new_x, new_y):
-    global min_y,min_x
-    return min_x <= new_x <= max_x and min_y <= new_y <= max_y
 
-def keep_track_of_sprint(input_from_user, x, y, name, direction, steps):
+def keep_track_of_sprint(input_from_user, x, y, robot_name, direction, steps):
     """
     the value will decrease by one until it reaches one
     it will take one less step forward
     """
     if steps > 0:
         input_from_user = f"sprint {steps}"
-        x, y, direction = robot_move(input_from_user, name, direction, x, y)
+        x, y, direction, instruction, oldx, oldy = \
+        moving_forward(input_from_user, robot_name, direction, x, y)
         steps = steps - 1
-        return keep_track_of_sprint(input_from_user, x, y, name, direction, steps)
+        return keep_track_of_sprint(input_from_user, x, y, robot_name, direction, steps)
     else:
-        return x,y
-
-
-def keep_track_of_position(x, y,robot_name, direction):
-    """
-    this function soley focuses on the position of the robot
-    current position is stored and a print statement will be printed
-    otherwise
-    (since the robot is allowed to move within certain parameters)
-    if the robots future position is outside the boundries, it does not move and
-    will notify the user
-    """
-    if x > -101 and x < 101 and y > -201 and y < 201:
-        print(" > {} now at position ({},{}).".format(robot_name, x, y))  
-    else:
-        print("{}: Sorry, I cannot go outside my safe zone.".format(robot_name))
-        print(f" > {robot_name} now at position ({x},{y}).")
-        return x, y, direction
+        return x, y, input_from_user
 
 
 def direction_left(direction, robot_name, x, y):
@@ -172,131 +130,88 @@ def direction_right(direction, robot_name, x, y):
     return direction
 
 
-def robot_move(robot_name, answer):
-    answer = answer.split()
-    global x, y
-    if current_rotation == "forward":
-        if answer[0] == "forward":
-            y = y + int(answer[1])
-        if answer[0] == "back":
-            y = y - int(answer[1])
-    elif current_rotation == "back":
-        if answer[0] == "forward":
-            y = y - int(answer[1])
-        if answer[0] == "back":
-            y = y + int(answer[1])
-    elif current_rotation == "left":
-        if answer[0] == "forward":
-            x = x - int(answer[1])
-        if answer[0] == "back":
-            x = x + int(answer[1])
-    elif current_rotation == "right":
-        if answer[0] == "forward":
-            x = x + int(answer[1])
-        if answer[0] == "back":
-            x = x - int(answer[1])
-    print(f" > {robot_name} moved {answer[0]} by {answer[1]} steps.")
-    print(f" > {robot_name} now at position ({x},{y}).")
-
-
-def do_right_turn(robot_name):
+def need_help():
     """
-    Do a 90 degree turn to the right
-    :param robot_name:
-    :return: (True, right turn output text)
+    when "help" is typed in by the user it will print out the help variable
     """
-    global current_rotation
+    help = """I can understand these commands:
+OFF  - Shut down robot
+HELP - provide information about commands
+FORWARD - in the direction that one is facing or travelling
+BACK - in the direction that one is not facing or travelling
+SPRINT - accelerates in the direction that one is not facing or travelling
+LEFT - turns left
+RIGHT - turns right"""
 
-    current_rotation += 1
-    if current_rotation > 4:
-        current_rotation = 0
-
-    return True, ' > '+robot_name+' turned right.'
+    print(help)
+    return help
 
 
-def direction_left(robot_name, x, y):
+def off_command(robot_name):
     """
-    anti-clockwise 
+    when "off" is the user input, this function runs
+    returns false to prevent it from running
     """
-    global current_rotation
-    current_rotation = current_rotation - 1
-    if current_rotation < -4:
-        current_rotation = 1
-    print(f" > {robot_name} turned left.")
-    print(" > {} now at position ({},{}).".format(robot_name, x, y))
-    return current_rotation
+    print(f"{robot_name}: Shutting down..")
+    return False
 
 
-def direction_right(robot_name, x, y):
+def invalid_command(user_input, robot_name):
     """
-    clockwise
-    x - position on the x-axis
-    y - position on the y-axis
+    as long as user input is not a valid command, the below will be printed
     """
-    global current_rotation
-    current_rotation = current_rotation + 1
-    if current_rotation > 4:
-        current_rotation = 1
-    print(f" > {robot_name} turned right.")
-    print(" > {} now at position ({},{}).".format(robot_name, x, y))
-    return current_rotation
+    print(f"{robot_name}: Sorry, I did not understand '{user_input}'.")
+    robot_start
 
 
-def do_left_turn(robot_name):
+def robot_start():
     """
-    Do a 90 degree turn to the left
-    :param robot_name:
-    :return: (True, left turn output text)
+    This is where the robot starts running
+    a certain set of commands is laid
+    Redirects the users input to the command
     """
-    global current_rotation
+    x = 0
+    y = 0
+    direction = 1
 
-    current_rotation -= 1
-    if current_rotation < 0:
-        current_rotation = 3
+    commands = ["off", "help", "forward", "back", "right", "up", "left", "down", "sprint"]
+    robot_on = True
+    """
+    as long as it is true, robot will continue running
+    """
+    robot_name = name_the_robot()
+    while robot_on:
+        """
+        user input is split and searches for specific command
+        """
+        input_from_user, new_input = get_command_input(robot_name)
+        cmd = input_from_user.split(" ")
 
-    return True, ' > '+robot_name+' turned left.'
+        if "help" in input_from_user:
+            need_help()
+        elif "off" in input_from_user:
+            robot_on = off_command(robot_name)
+        elif "forward" == cmd[0] and cmd[1].isdigit():
+            x, y, direction, instruction, oldx, oldy = \
+            moving_forward(input_from_user, robot_name, direction, x, y )
+            x, y, direction = \
+            keep_track_of_position(x, y, int(cmd[1]), robot_name, direction, oldx, oldy)
+        elif "back" == cmd[0] and cmd[1].isdigit():
+            x, y , direction, instruction, oldx, oldy = \
+            moving_backward(input_from_user, robot_name, direction, x, y )
+            x, y, direction = \
+            keep_track_of_position(x, y, int(cmd[1]), robot_name, direction, oldx, oldy)
+        elif input_from_user == "left":
+            direction = direction_left(direction, robot_name, x, y)
+        elif input_from_user == "right":
+            direction = direction_right(direction, robot_name, x, y)
+        elif "sprint" in cmd and cmd[1].isdigit():
+            x, y, input_from_user = \
+            keep_track_of_sprint(input_from_user, x, y, robot_name, direction, int(cmd[1]))
+            print(" > {} now at position ({},{}).".format(robot_name, x, y))
+        else:
+            invalid_command(new_input, robot_name)
 
 
-# def do_sprint(robot_name, steps):
-#     """
-#     Sprints the robot, i.e. let it go forward steps + (steps-1) + (steps-2) + .. + 1 number of steps, in iterations
-#     :param robot_name:
-#     :param steps:
-#     :return: (True, forward output)
-#     """
-
-#     if steps == 1:
-#         return forward(robot_name, 1)
-#     else:
-#         (do_next, command_output) = do_forward(robot_name, steps)
-#         print(command_output)
-#         return do_sprint(robot_name, steps - 1)
-
-
-# def robot_left_or_right(answer,robot_name):
-#     answer = answer.split()
-#     global x, y
-#     if answer[0] == "left":
-#         x = x -int(answer[0])
-#     if answer[0] == "right":
-#         x = x +int(answer[0])
-#     print(f" > {robot_name} moved {answer[0]} by {answer[1]} steps.")
-#     print(f" > {robot_name} now at position ({x},{y}).")
-
-
-#def forward_tracing():
-
-# def robot_directions(answer):
-#     if answer == 
-
-# def gets_commands(robot_name,answer):
-#     # arr_cmd=line.split()
-#     # command=arr_cmd[0]
-#     answer = answer.split()
-#     global x,y
-#     if (answer=='left'):
-#         x = x + int(answer[0])
-#         print(f" > {robot_name} moved {answer[0]} by {answer[1]} steps.")
-#         print(f" > {robot_name} now at position ({x},{y}).")
 if __name__ == "__main__":
     robot_start()
--- test_robot.py
+++ test_robot.py
@@ -1,16 +1,67 @@
-# import unittest
-# import os
-# import sys
-# from io import StringIO
-# from test_base import captured_io
-# from unittest.mock import patch
-# from test_base import run_unittests
-# class TestRobot(unittest.TestCase):
-#     def test_robot_move(self):
-#         self.assertEqual()
-#     def test_keep_track_of_position(self):
-#         self.assertEqual()
-#     def test_keep_track_of_sprint(self):
-#         self.assertEqual()
-#     def test_show_position(self):
-#         self.assertEqual
+import unittest
+from io import StringIO
+from robot import *
+from unittest.mock import patch
+
+@patch("sys.stdout", StringIO())
+class TestSuperAlgos(unittest.TestCase):
+    
+    @patch("sys.stdin", StringIO("Hal\n"))
+    def test_name_input(self):
+        output = "Hal"
+        self.assertTrue(name_the_robot(), output)
+
+
+    @patch("sys.stdin", StringIO("right\n"))
+    def test_right_movement(self):
+        output = 2
+        name = "Hal"
+        x = 10
+        y = 10
+        direction = 1
+        self.assertEqual(direction_right(direction, name, x, y), output)
+    
+
+    @patch("sys.stdin", StringIO("left\n"))
+    def test_left_movement(self):
+        output = 0
+        name = "Hal"
+        x = 10
+        y = 10
+        direction = 1
+        self.assertEqual(direction_left(direction, name, x, y), output)
+    
+
+    def test_forward_movement(self):
+        output = "forward"
+        input_from_user = "forward 10"
+        name = "Hal"
+        direction = 1
+        x = 10
+        y = 10
+        self.assertEqual(moving_forward(input_from_user, name, direction, x, y)[3], output)
+
+
+    def test_back_command(self):
+        output = "back"
+        input_from_user = "back 10"
+        name = "Hal"
+        direction = 1
+        x = 10
+        y = 10
+        self.assertEqual(moving_backward(input_from_user, name, direction, x, y)[3], output)
+    
+
+    @patch("sys.stdin", StringIO("Hal\nsprint 5\noff\n"))
+    def test_sprint(self):
+        output = "sprint 1"
+        input_from_user = "sprint 5"
+        x = 0
+        y = 0
+        name = "Hal"
+        direction = 1
+        steps = 5
+        self.assertEqual(keep_track_of_sprint(input_from_user, x, y, name, direction, steps)[2], output)
+    
+if __name__ == '__main__':
+    unittest.main()
